75b8c779607026c092ac057fd730358e5bba33b9
dev-cozeto <victorcozeto@gmail.com>
2021-09-17 22:50:31 -0300

    Classes de domínio iniciadas

---
+++ src/main/java/com/bs/bookstore/domain/Categoria.java
@@ -0,0 +1,85 @@
+package com.bs.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Categoria {
+
+	// Declaração de variáveis
+	private Integer id;
+	private String nome;
+	private String descricao;
+
+	// Declaração da Lista
+	private List<Livro> livros = new ArrayList<>();
+
+	// Construtores
+	public Categoria() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	// Construtor com parâmetros
+	public Categoria(Integer id, String nome, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	// Métodos acessores e modificadores (Getters e Setters)
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+	public void setLivros(List<Livro> livros) {
+		this.livros = livros;
+	}
+
+	// hashCode equals para ter os métodos comparadores
+	// Será comparado somente com o id
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
